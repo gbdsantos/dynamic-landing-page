b5760a0be5ddb9bcfc444d04a4a0beb7251ae7c8
Guilherme Bezerra <gbsantos.it@gmail.com>
2019-06-15 22:45:45 -0300

    [dynamic-landing-page] Finish Project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // DOM Elements
-const time = document.getElementById('time');
-  greeting = document.getElementById('greeting');
-  name = document.getElementById('name');
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
   focus = document.getElementById('focus');
+
+// Option
+const showAmPm = true;
 
 // Show Time
 function showTime() {
@@ -18,7 +21,7 @@ function showTime() {
   hour = hour % 12 || 12;
 
   // Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
  
   setTimeout(showTime, 1000); 
 }
@@ -49,6 +52,56 @@ function setBgGreet() {
   }
 }
 
+// Get Name
+function getName() {
+  if (localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Name]';
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
+}
+// Set Name
+function setName(e) {
+  if (e.type === 'keypress') {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+  }
+}
+
+// Get Focus
+function getFocus() {
+    if(localStorage.getItem('focus') === null) {
+      focus.textContent = '[Enter Focus]';    
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// Set Focus
+function setFocus(e) {
+    if (e.type === 'keypress') {
+      // Make sure enter is pressed
+      if (e.which == 13 || e.keyCode == 13) {
+        localStorage.setItem('focus', e.target.innerText);
+        focus.blur();
+      }
+    } else {
+      localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
 // Run
 showTime();
 setBgGreet();
+getName();
+getFocus();
